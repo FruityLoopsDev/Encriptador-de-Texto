ce38484dd80d7b9ba549ba68034325c299ecf0e0
Dall-ECruz <cesarcruztrader@gmail.com>
2023-06-08 20:02:00 -0400

    Estructura HTML, CSS y funcion Desencriptar finalizadas

---
+++ encriptador.js
@@ -0,0 +1,14 @@
+function encriptar(){
+
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase();
+
+    var textoEncriptado = frase.replace(/e/img, "enter");
+    var textoEncriptado = textoEncriptado.replace(/o/img, "ober");
+    var textoEncriptado = textoEncriptado.replace(/i/img, "imes");
+    var textoEncriptado = textoEncriptado.replace(/a/img, "ai");
+    var textoEncriptado = textoEncriptado.replace(/u/img, "ufat");
+
+    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
+
+    
+}
